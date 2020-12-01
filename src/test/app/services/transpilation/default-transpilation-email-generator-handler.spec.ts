d15fcc999cf714e931a0cb62996366e7fa6b29c0
Michael Cabana <m.cabana@ieg-america.com>
2020-12-01 17:03:07 -0500

    test: added missing tests

---
+++ src/test/app/services/transpilation/default-transpilation-email-generator-handler.spec.ts
@@ -0,0 +1,28 @@
+import { IEmailGeneratorHandler } from "../../../../abstraction/services/handlers";
+import { DependencyInjection } from "../../../../app/internal";
+import { DefaultTranspilationEmailGeneratorHandler } from "../../../../app/services/handlers/transpilation";
+
+describe('DefaultTranspilationEmailGeneratorHandler Test Suite', () => {
+
+    beforeAll(() => {
+        DependencyInjection.configureServices();
+    });
+
+    afterAll(() => {
+        const container = DependencyInjection.serviceProvider();
+        container.reset();
+    });
+
+    it('New instance should not throw', () =>
+        expect(() => {
+            DependencyInjection.serviceProvider().resolveAll<IEmailGeneratorHandler>('IEmailGeneratorHandler')
+        }).not.toThrow()
+    )
+
+    it('There should be 1 instance', () => {
+        const handlers = DependencyInjection.serviceProvider().resolveAll<IEmailGeneratorHandler>('IEmailGeneratorHandler');
+        const filtered = handlers.filter(h => h instanceof DefaultTranspilationEmailGeneratorHandler);
+        expect(filtered).toHaveSize(1);
+    })
+
+})
